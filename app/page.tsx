d32d20f4ad6d57e0b399ecfbd13f11033af601a9
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-12-06 18:51:31 +0200

    texting hero section2

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
         <div className="fixed top-0 left-0 w-full h-2/6 overflow-hidden -z-10">
           <Image src="/about.jpg" alt="Hero Image" width={600} height={400} className="absolute -top-2/6" />
         </div>
-        <div className=" h-full flex flex-col justify-center items-start gap-6 p-8">
+        <div className=" h-full flex flex-col justify-end items-start gap-4 p-8 -translate-y-1/6">
           <h1 className="text-7xl font-bold text-text-main font-heading">
           MAGOMY
         </h1>
-        <h2 className="text-4xl font-bold text-accent font-heading">
+        <h2 className="text-4xl font-bold text-accent font-heading -mt-4">
           Video Editor & Filmmaker
         </h2>
         <p className="text-xl font-body">
           Helping creators turn raw footage into cinematic storytelling.
         </p>
-        <button className="py-2 px-5 bg-cards">Let's Talk</button>
+        <button className="py-2 px-5 bg-cards border border-accent">Let's Talk</button>
         </div>
       </main>
   );
