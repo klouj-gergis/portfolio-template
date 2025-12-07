96622c257a02a1a6332a2a26329e7c7d94102cbd
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-12-07 18:59:28 +0200

    fixing the spacing in the hero section 4

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Line from "@/components/Line";
 
 export default function Home() {
   return (
-      <main className="w-full *:min-h-screen flex flex-col">
+      <main className="w-full *:min-h-screen flex flex-col relative bg-background -z-20">
         <Hero />
         <About />
       </main>
