c9966811d3e1ed2e846e88bae61ceb9ae3afbaea
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-12-09 16:16:36 +0200

    Create or update page.tsx with contact component in main branch.

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from './components/Contact';
+
+export default function Page() {
+  return (
+    <main>
+      {/* Other page content */}
+      <Contact />
+      {/* Other sections/components */}
+    </main>
+  );
+}
