8e6776da3db351f0fd23bc16138a43e1bd3345cf
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-12-17 21:14:27 +0200

    changing testimonials layout on mobile

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -3,13 +3,13 @@ import TestimonialCard from "@/components/TestimonialCard"
 
 export default function TestimonialsSection() {
   return (
-    <section className="w-full h-52 border-y my-40 overflow-hidden relative">
-      <ul className="flex items-center h-full w-max animate-scroll hover:[animation-play-state:paused]">
+    <section className="w-full h-screen lg:h-52 lg:flex items-center border-y lg:mt-20 overflow-hidden relative">
+      <ul className="flex flex-wrap lg:flex h-max w-max lg:animate-scroll lg:hover:[animation-play-state:paused]  relative">
         
         {/* FIRST TRACK */}
        
           {testimonials.map((t, i) => (
-            <li key={i} className="shrink-0 ml-5">
+            <li key={i} className="shrink-0 ml-5 col-span-1">
               <TestimonialCard {...t} />
             </li>
           ))}
@@ -18,15 +18,15 @@ export default function TestimonialsSection() {
         {/* SECOND TRACK */}
         
           {testimonials.map((t, i) => (
-            <li key={`b-${i}`} className="shrink-0 ml-5">
+            <li key={`b-${i}`} className="hidden lg:block shrink-0 ml-5">
               <TestimonialCard {...t} />
             </li>
           ))}
         
 
       </ul>
-      <div className="absolute top-0 left-0 h-full bg-linear-to-r from-black to-black/0 w-1/12"></div>
-      <div className="absolute top-0 right-0 h-full bg-linear-to-l from-black to-black/0 w-1/12"></div>
+      <div className="hidden lg:block absolute top-0 left-0 h-full bg-linear-to-r from-black to-black/0 w-1/12"></div>
+      <div className="hidden lg:block absolute top-0 right-0 h-full bg-linear-to-l from-black to-black/0 w-1/12"></div>
     </section>
   )
 }
