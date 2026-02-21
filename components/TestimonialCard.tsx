6ea78a50f18ee955234934ad5942b29bf0b5ee21
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2026-02-21 07:00:35 +0200

    Refactor TestimonialCard to use img instead of Image

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -3,20 +3,20 @@ import Image from "next/image"
 
 export default function TestimonialCard({ name, role, image, quote}: TestimonialType) {
   return (
-    <div className=" w-full flex flex-col items-start bg-primary rounded-2xl p-3">
-      <div className="flex items-center gap-3">
+    <div className=" w-96 h-full flex flex-col items-start bg-primary rounded-2xl p-3">
+      <div className="h-fit flex items-center gap-3">
         <div className="w-max h-15 overflow-hidden rounded-full">
-          <Image src={image} height={60} width={60} alt="testimonial image" />
+          <img src={image} height={60} width={60} alt="testimonial image" />
         </div>
         <div>
           <h3 className="text-2xl font-semibold text-accent">{name.toUpperCase()}</h3>
           <span className="text-text-secondary">{role.toUpperCase()}</span>
         </div>
-        
+
       </div>
       <div className="w-full max-w-86 lg:max-w-96 p-3 px-5">
-        <p>{quote}</p>
-        
+        <p className="text-text-main">{quote}</p>
+
       </div>
     </div>
   )
