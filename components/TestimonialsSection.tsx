7a6ef5c3c6e91dc4540b496bab3b2ac9f3afe4c0
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2026-02-21 07:05:31 +0200

    Refactor TestimonialsSection layout and structure

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -3,44 +3,39 @@ import TestimonialCard from "@/components/TestimonialCard"
 
 export default function TestimonialsSection() {
   return (
-    <section className=" border-y flex flex-col items-center py-5 lg:gap-4">
+   <section className=" border-y h-fit flex flex-col items-center py-5 lg:gap-4 mt-5 overflow-hidden">
       <h2 className="text-5xl lg:text-5xl text-accent font-semibold font-heading lg:tracking-wider">What Our Clients Say</h2>
-      <div className="w-full px-5 lg:px-0 py-10 lg:h-52 flex lg:flex-col items-center">
+      <div className="w-full px-5 lg:px-0 py-10 lg:h-full flex lg:flex-col items-center">
         {/* MOBILE */}
-        <div className="w-full lg:hidden flex flex-col gap-4 items-center px-2">
+        <div className="w-full lg:hidden h-full flex flex-col gap-4 items-center px-2">
           {testimonials.map((t, i) => (
              <TestimonialCard key={i} {...t}/>
           ))}
         </div>
   {/* DESKTOP MARQUEE */}
-  <div className="relative hidden lg:block w-full overflow-hidden">
-    
-    <ul
-      className="
-        flex w-max gap-4
+  <div className="relative hidden h-full lg:flex w-max gap-4
         animate-[scroll_30s_linear_infinite]
-        hover:[animation-play-state:paused]
-        will-change-transform
-      "
-    >
+        hover:paused
+        will-change-transform">
+
+    <div className="flex gap-4">
       {testimonials.map((t, i) => (
-        <li key={i} className="shrink-0">
-          <TestimonialCard {...t} />
-        </li>
+        
+         <TestimonialCard key={i} {...t}/>
       ))}
+      </div>
+         <div className="flex gap-4">
       {testimonials.map((t, i) => (
-        <li key={`dup-${i}`} className="shrink-0">
-          <TestimonialCard {...t} />
-        </li>
+        
+          <TestimonialCard key={i} {...t}/>
+          
       ))}
-    </ul>
+    </div>
 
   </div>
       </div>
-  
-  
+
+
 </section>
-
-
   )
 }
